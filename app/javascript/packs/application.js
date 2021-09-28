d7bd451ae2be637321ed3e458fc31290743dedd1
vivian-bb <yuweizhang88@gmail.com>
2021-09-28 15:33:53 +1000

    fix the push heroku failed error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,17 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'bootstrap'
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+// import "bootstrap"
+// require("@rails/ujs").start()
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
 //= require jquery3
 //= require popper
 //= require bootstrap
 
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack');
-environment.plugins.append('Provide', 
-    new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        Poppery: ['popper.js', 'default']
-    })
-    );
+const webpack = require('webpack')
+
+environment.plugins.append('Provide',
+new webpack.ProvidePlugin({
+$: 'jQuery',
+jQuery: 'jQuery',
+Popper: ['popper.js', 'default'] // Do not include for Rails 6.1.x
+})
+)
 
 module.exports = environment
